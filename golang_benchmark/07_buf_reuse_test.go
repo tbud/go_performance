9c559da90eaa372935d5afba2c5acba85cff7378
peace0phmind <peace0phmind@gmail.com>
2015-05-13 21:58:18 +0800

    add buf reuse test

---
+++ golang_benchmark/07_buf_reuse_test.go
@@ -0,0 +1,62 @@
+package golang_benchmark
+
+import (
+	"math/rand"
+	"testing"
+)
+
+func makeBuffer() []byte {
+	return make([]byte, rand.Intn(5000000)+5000000)
+}
+
+func Benchmark07NormalMake(b *testing.B) {
+	pool := make([][]byte, 20)
+	for i := 0; i < b.N; i++ {
+		b := makeBuffer()
+
+		i := rand.Intn(len(pool))
+		pool[i] = b
+
+		bytes := 0
+
+		for i := 0; i < len(pool); i++ {
+			if pool[i] != nil {
+				bytes += len(pool[i])
+			}
+		}
+	}
+}
+
+func Benchmark07ChanReuse(b *testing.B) {
+	pool := make([][]byte, 20)
+
+	buffer := make(chan []byte, 5)
+
+	for i := 0; i < b.N; i++ {
+		var b []byte
+		select {
+		case b = <-buffer:
+		default:
+			b = makeBuffer()
+		}
+
+		i := rand.Intn(len(pool))
+		if pool[i] != nil {
+			select {
+			case buffer <- pool[i]:
+				pool[i] = nil
+			default:
+			}
+		}
+
+		pool[i] = b
+
+		bytes := 0
+
+		for i := 0; i < len(pool); i++ {
+			if pool[i] != nil {
+				bytes += len(pool[i])
+			}
+		}
+	}
+}
