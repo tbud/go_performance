4b061ad5eb36fcdfa01ccc9ed41c88be5331fbb6
peace0phmind <peace0phmind@gmail.com>
2015-05-13 22:15:05 +0800

    add use pool

--- golang_benchmark/07_buf_reuse_test.go
+++ golang_benchmark/07_buf_reuse_test.go
@@ -2,11 +2,16 @@ package golang_benchmark
 
 import (
 	"math/rand"
+	"sync"
 	"testing"
 )
 
 func makeBuffer() []byte {
 	return make([]byte, rand.Intn(5000000)+5000000)
+}
+
+type bufferPool struct {
+	b []byte
 }
 
 func Benchmark07NormalMake(b *testing.B) {
@@ -48,6 +53,34 @@ func Benchmark07ChanReuse(b *testing.B) {
 			default:
 			}
 		}
+
+		pool[i] = b
+
+		bytes := 0
+
+		for i := 0; i < len(pool); i++ {
+			if pool[i] != nil {
+				bytes += len(pool[i])
+			}
+		}
+	}
+}
+
+func Benchmark07PoolReuse(b *testing.B) {
+	pool := make([][]byte, 20)
+
+	p := sync.Pool{
+		New: func() interface{} {
+			return make([]byte, rand.Intn(5000000)+5000000)
+		},
+	}
+
+	for i := 0; i < b.N; i++ {
+		b := p.Get().([]byte)
+
+		i := rand.Intn(len(pool))
+
+		p.Put(pool[i])
 
 		pool[i] = b
 
