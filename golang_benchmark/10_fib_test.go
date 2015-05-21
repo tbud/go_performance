2503bdc00b0ce5027b71731f8df0f819d36c1da6
peace0phmind <peace0phmind@gmail.com>
2015-05-21 23:10:16 +0800

    add fib test

---
+++ golang_benchmark/10_fib_test.go
@@ -0,0 +1,24 @@
+package golang_benchmark
+
+import "testing"
+
+func Fib(n int) int {
+	if n < 2 {
+		return n
+	}
+	return Fib(n-1) + Fib(n-2)
+}
+
+var result int
+
+func BenchmarkFibComplete(b *testing.B) {
+	var r int
+	for n := 0; n < b.N; n++ {
+		// always record the result of Fib to prevent
+		// the compiler eliminating the function call.
+		r = Fib(10)
+	}
+	// always store the result to a package level variable
+	// so the compiler cannot eliminate the Benchmark itself.
+	result = r
+}
